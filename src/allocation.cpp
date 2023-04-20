1fc20f00470184095bb27b7eb2242ee4b9d4104f
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-04-20 21:25:49 +0200

    Add default allocator with desired interface

---
+++ src/allocation.cpp
@@ -0,0 +1,20 @@
+#include "allocation.hpp"
+#include <cstdlib>
+
+#include <malloc.h>
+#define ALLOCATED_SIZE(ptr) malloc_usable_size(ptr)
+// _msize on Windows
+// malloc_size on MacOS
+
+gstd::allocation_result<void> gstd::default_allocator::do_allocation(gstd::size_t size) noexcept {
+	auto ptr = std::malloc(size);
+	return {ptr, ALLOCATED_SIZE(ptr)};
+}
+
+void * gstd::default_allocator::do_reallocation(void * ptr, gstd::size_t new_size) noexcept {
+	return std::realloc(ptr, new_size);
+}
+
+void gstd::default_allocator::do_deallocation(void * ptr) noexcept {
+	std::free(ptr);
+}
