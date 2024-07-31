72d65fc0ab4a6e66d3c19599c046b1781aac971d
Gereon V <72784429+GereonV@users.noreply.github.com>
2024-07-31 22:09:12 +0200

    Add always_false template
    Workaround for static_assert(false) before C++23

---
+++ include/utility/static_false.hpp
@@ -0,0 +1,26 @@
+#ifndef GSTD_STATIC_FALSE_HPP
+#define GSTD_STATIC_FALSE_HPP
+
+// Until C++23, `static_assert(false)` is always ill-formed.
+// Use `static_assert(GSTD_STATIC_FALSE(T))` with some template-dependant type `T` as a workaround!
+// Further information: https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2022/p2593r0.html
+
+
+#if __cplusplus >= 202101 && (__clang_major__ >= 17 || __GNUC__ >= 13)
+#define GSTD_STATIC_FALSE(T) false
+#else
+#define GSTD_STATIC_FALSE(T) gstd::utility::static_false::always_false_v<T>
+
+namespace gstd::utility::static_false {
+    // This mustn't be specialized!
+    // However, it needs to be specializable for the semantics to work.
+    template<typename...>
+    struct always_false {
+        static constexpr bool value = false;
+    };
+
+    template<typename... T>
+    inline constexpr bool always_false_v = always_false<T...>::value;
+}
+#endif
+#endif
