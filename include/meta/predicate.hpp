e863a0c80891c5a9850a10ff764c66b6b4f5afaf
Gereon V <72784429+GereonV@users.noreply.github.com>
2024-08-01 14:45:09 +0200

    Add lambda-to-predicate utility

--- include/meta/predicate.hpp
+++ include/meta/predicate.hpp
@@ -2,6 +2,7 @@
 #define GSTD_META_PREDICATE_HPP
 
 #include <concepts>
+#include <type_traits>
 
 // A meta-programming (unary) predicate `Pred` is a class template that assigns a `Pred<T>::value` to types `T`.
 // Special consideration is required if `Pred<T>::value` doesn't exist, often it is interpreted as `false`.
@@ -37,19 +38,15 @@ namespace gstd::meta::predicate {
     template<template<typename> typename Pred, bool Default = false>
     struct completed {
         template<_impl::predicated_by<Pred> T>
-        struct type {
-            static constexpr bool value = Default;
-        };
+        struct type : std::bool_constant<Default> {};
 
         template<typename T>
         requires _impl::has_bool_value<Pred<T>>
-        struct type<T> {
-            static constexpr bool value = Pred<T>::value;
-        };
+        struct type<T> : Pred<T> {};
     };
 
     // `negated<Pred>::type<T>::value == !Pred<T>::value`
-    // SFINAE: if `Pred<T>::value` doesn't exists, `negated<Pred><T>::value` doesn't either
+    // SFINAE: if `Pred<T>::value` doesn't exist, `negated<Pred><T>::value` doesn't either
     template<template<typename> typename Pred>
     struct negated {
         template<_impl::predicated_by<Pred> T>
@@ -57,14 +54,25 @@ namespace gstd::meta::predicate {
 
         template<typename T>
         requires _impl::has_bool_value<Pred<T>>
-        struct type<T> {
-            static constexpr bool value = !Pred<T>::value;
-        };
+        struct type<T> : std::bool_constant<!Pred<T>::value> {};
     };
 
     // `inverted<Pred>::type<T>::value == negated<completed<Pred>::type>::type<T>::value`
     template<template<typename> typename Pred>
     struct inverted : negated<completed<Pred>::template type> {};
+
+    // uses return-type of templated `operator()`
+    template<auto Lambda>
+    struct from_lambda {
+        template<typename T>
+        struct type {};
+
+        template<typename T>
+        requires requires { Lambda.template operator()<T>(); }
+        struct type<T> : decltype(Lambda.template operator()<T>()) {
+            static_assert(_impl::has_bool_value<type>, "invalid return type");
+        };
+    };
 
     template<template<typename> typename Pred, typename T, bool Default = false>
     inline constexpr bool invoke = completed<Pred, Default>::template type<T>::value;
