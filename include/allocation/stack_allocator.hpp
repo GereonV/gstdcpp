0f31b6589f6e2a83e2c7a834e85569996bd5b217
Gereon V <72784429+GereonV@users.noreply.github.com>
2024-08-06 21:41:07 +0200

    Add stack_allocator

---
+++ include/allocation/stack_allocator.hpp
@@ -0,0 +1,31 @@
+#ifndef GSTD_ALLOCATION_STACK_ALLOCATION_HPP
+#define GSTD_ALLOCATION_STACK_ALLOCATION_HPP
+
+#include "allocation/base.hpp"
+#include "allocation/buffer_allocator.hpp"
+
+namespace gstd::allocation {
+    namespace _impl {
+        template<size_t N, size_t StartAlignment = 1>
+        class stack_allocator_base {
+          public:
+            stack_allocator_base()                             = default;
+            stack_allocator_base(stack_allocator_base const &) = delete;
+            stack_allocator_base(stack_allocator_base &&)      = delete;
+            void operator=(stack_allocator_base const &)       = delete;
+            void operator=(stack_allocator_base &&)            = delete;
+          protected:
+            constexpr allocation_result * get_allocation() const noexcept { return {_data, N}; }
+          private:
+            alignas(StartAlignment) char _data[N];
+        };
+    }
+
+    template<size_t N, size_t StartAlignment = 1>
+    struct stack_allocator : _impl::stack_allocator_base<N, StartAlignment>,
+                             _impl::buffer_allocator_base<stack_allocator<N, StartAlignment>> {
+        friend _impl::buffer_allocator_base<stack_allocator<N, StartAlignment>>;
+    };
+}
+
+#endif
