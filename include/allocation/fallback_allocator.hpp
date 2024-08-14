716da5a53634b1b7e6990927f9e156e4181fdb4e
Gereon V <72784429+GereonV@users.noreply.github.com>
2024-08-14 16:39:51 +0200

    Use std::make_from_tuple instead of DIY variant

--- include/allocation/fallback_allocator.hpp
+++ include/allocation/fallback_allocator.hpp
@@ -26,17 +26,15 @@ namespace gstd::allocation {
             : _primary{static_cast<T &&>(primary)}, _fallback{static_cast<U &&>(fallback)}
         {}
 
-        // use with std::forward_as_tuple
-        template<typename... PrimaryArgs, typename... FallbackArgs>
-        constexpr fallback_allocator(
-          std::piecewise_construct_t,
-          std::tuple<PrimaryArgs...> p,
-          std::tuple<FallbackArgs...> f
-        ) noexcept(std::is_nothrow_constructible_v<Primary, PrimaryArgs...> && std::is_nothrow_constructible_v<Fallback, FallbackArgs...>)
-            : fallback_allocator{
-                p, f, meta::type_sequence::indices<sizeof...(PrimaryArgs)>{},
-                meta::type_sequence::indices<sizeof...(FallbackArgs)>{}
-              }
+        // eg. usable with std::forward_as_tuple
+        template<typename PrimaryArgsTup, typename FallbackArgsTup>
+        constexpr fallback_allocator(std::piecewise_construct_t, PrimaryArgsTup && p, FallbackArgsTup && f) noexcept(
+          // all big STLs voluntarily provide correct `noexcept`-specification
+          noexcept(std::make_from_tuple<Primary>(static_cast<PrimaryArgsTup &&>(p))) && //
+          noexcept(std::make_from_tuple<Fallback>(static_cast<FallbackArgsTup &&>(f)))
+        )
+            : _primary{std::make_from_tuple<Primary>(static_cast<PrimaryArgsTup &&>(p))}
+            , _fallback{std::make_from_tuple<Fallback>(static_cast<FallbackArgsTup &&>(f))}
         {}
 
         constexpr allocation_result allocate(size_t size) noexcept
@@ -93,13 +91,6 @@ namespace gstd::allocation {
             return _primary.owns(allocation) || _fallback.owns(allocation);
         }
       private:
-        template<typename PTup, typename FTup, meta::type_sequence::size_t... PI, meta::type_sequence::size_t... FI>
-        constexpr fallback_allocator(PTup & p, FTup & f, meta::type_sequence::index_sequence<PI...>, meta::type_sequence::index_sequence<FI...>) noexcept(
-          noexcept(Primary{get<PI>(std::move(p))...}) && noexcept(Fallback{get<FI>(std::move(f))...})
-        )
-            : _primary{get<PI>(std::move(p))...}, _fallback{get<FI>(std::move(f))...}
-        {}
-
         [[no_unique_address]] Primary _primary;
         [[no_unique_address]] Fallback _fallback;
     };
